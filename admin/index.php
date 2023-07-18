445bb517c72ab811fc088a5e400ebf7832c6ecad
jorge <jorgefreire2403@gmail.com>
2023-07-18 14:49:29 -0400

    first commit

---
+++ admin/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MiTienda</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Mi</b>Tienda</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Iniciar Session</p>
+
+      <?php
+      if(isset($_REQUEST['login'])){
+        session_start();
+        $email=$_REQUEST['email']??'';
+        $pasword=$_REQUEST['pass']??'';
+        $pasword=md5($pasword);
+        include_once "db_tienda.php";
+        $con=mysqli_connect($host,$user,$pass,$db);
+        $query="SELECT id,email,nombre FROM usuarios where email= '".$email."' and pass= '".$pasword."';";
+        $res=mysqli_query($con,$query);
+        $row=mysqli_fetch_assoc($res);
+        if($row){
+          $_SESSION['id']=$row['id'];
+          $_SESSION['email']=$row['email'];
+          $_SESSION['nombre']=$row['nombre'];
+          header("location: panel.php");
+        }else{
+          ?>
+          <div class="alert alert-danger" role="alert">
+            <strong>Error al iniciar</strong>
+          </div>
+          <?php
+        }
+      }
+      ?>
+
+      <form method="post">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email" name="email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" name="pass">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block" name="login">Ingresar</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <!-- /.social-auth-links -->
+      <p class="mb-0">
+        <a href="register.html" class="text-center">Register a new membership</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
---
+++ admin/usuarios.php
@@ -0,0 +1,70 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Usuarios</h1>
+                </div>
+
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example2" class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Email</th>
+                                        <th>Acciones
+                                            <a href="panel.php?modulo=crearUsuario"><i class="fa fa-plus" aria-hidden="true"></i></a>
+                                        </th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                <?php
+                                include_once "db_tienda.php";
+                                $con=mysqli_connect($host,$user,$pass,$db);
+                                $query="SELECT id,email,nombre FROM usuarios;";
+                                $res=mysqli_query($con,$query);
+                                $row=mysqli_fetch_assoc($res);
+                                while($row = mysqli_fetch_assoc($res)){
+                                ?>
+                                    <tr>
+                                        <td><?php echo $row['nombre']?></td>
+                                        <td><?php echo $row['email']?></td>
+                                        <td>
+                                            <a href="editarUsuario.php?id=<?php echo $row['id']?>" style="margin-right: 5px;"> <i class="fas fa-edit    "></i></a>
+                                            <a href="usuarios.php?idBorrar=<?php echo $row['id']?>" class="text-danger"> <i class="fas fa-trash    "></i></a>
+                                        </td>
+
+                                    </tr>
+                                    <?php
+                                }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+
+
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
